eee78ceb56afaf150da2b0ad3fc27f49b7a571db
Matheus Wisch <wisch.pg@outlook.com>
2024-02-22 20:56:54 -0300

    feat: config db e etc

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using APICatalogo.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Context;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
     
+    public DbSet<Categoria>? Categoria { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
 }
