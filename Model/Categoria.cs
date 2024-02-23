9a07b6c63df602a6ddbf27da7ff66b9ca718709c
Matheus Wisch <wisch.pg@outlook.com>
2024-02-22 21:27:28 -0300

    feat: configurando relacionamento

--- Model/Categoria.cs
+++ Model/Categoria.cs
@@ -1,8 +1,16 @@
-﻿namespace APICatalogo.Model;
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Model;
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? imagemUrl { get;}
+    public ICollection<Produto> Produtos { get; set; }
 }
