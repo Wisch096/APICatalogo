e25115b24e73a1f48ad508184fd38ec5dfa1301f
Matheus Wisch <wisch.pg@outlook.com>
2024-02-25 19:35:21 -0300

    fix: att net 8

--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a07b6c63df602a6ddbf27da7ff66b9ca718709c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293d01b64a93d53dfa4a3dcfbdccbd02560bb60f")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
