5fa7c6e6dc5df8cf01d31b3832b60ffe27616392
Matheus Wisch <wisch.pg@outlook.com>
2024-02-25 19:48:07 -0300

    feat: aplicando migrations

--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81027f0a470b37d60a92d67e9de4c7f914579ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+400af2316b30cbd9a2e9cefcd183fe3dfbf35c03")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
