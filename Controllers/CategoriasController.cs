c362734910a2c67e48fd63adc5f3a997508092a2
Matheus Wisch <wisch.pg@outlook.com>
2024-02-27 20:47:48 -0300

    feat: criando controller de categorias

---
+++ Controllers/CategoriasController.cs
@@ -0,0 +1,37 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Model;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CategoriasController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public CategoriasController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Categoria>> Get()
+    {
+        return _context.Categoria.ToList();
+    }
+
+    [HttpGet("{id:int}", Name = "ObterCategoria")]
+    public ActionResult<Categoria> Get(int id)
+    {
+        var categoria = _context.Categoria.FirstOrDefault(p => p.CategoriaId == id);
+
+        if (categoria == null) 
+            return NotFound();
+
+        return Ok(categoria);
+    }
+    
+    
+}
