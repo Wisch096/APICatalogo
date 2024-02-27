555ad714650c951da6c09963998301a21d72c53f
Matheus Wisch <wisch.pg@outlook.com>
2024-02-27 20:38:40 -0300

    feat: criaçao método delete

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -56,11 +56,23 @@ public class ProdutosController : ControllerBase
     public ActionResult Put(int id, Produto produto)
     {
         if (id != produto.ProdutoId)
-        {
             return BadRequest();
-        }
 
         _context.Entry(produto).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        
+        if (produto is null)
+            return NotFound();
+
+        _context.Produtos.Remove(produto);
         _context.SaveChanges();
 
         return Ok(produto);
