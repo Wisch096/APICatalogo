950b2f2185683a001b98cea395fb35067ee37a2f
Matheus Wisch <wisch.pg@outlook.com>
2024-03-03 13:24:36 -0300

    feat: add validaçao no model

--- Model/Produto.cs
+++ Model/Produto.cs
@@ -6,7 +6,7 @@ using APICatalogo.Validations;
 namespace APICatalogo.Model;
 
 [Table(("Produtos"))]
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
@@ -32,4 +32,20 @@ public class Produto
     
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new
+                    ValidationResult("A primeira letra do produto deve ser maiúscula",
+                        new[]
+                            { nameof(this.Nome) }
+                    );
+            }
+        }
+    }
 }
