ada692bc7e37f0e74d6b5f04d27855351d733cce
Matheus Wisch <wisch.pg@outlook.com>
2024-03-07 20:27:17 -0300

    feat: melhorando muito o código novo com o repository

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -10,28 +10,38 @@ namespace APICatalogo.Controllers;
 [ApiController]
 public class ProdutosController : ControllerBase
 {
-    private readonly IProdutoRepository _repository;
+    private readonly IProdutoRepository _produtoRepository;
 
-    public ProdutosController(IProdutoRepository repository)
+    public ProdutosController(IProdutoRepository produtoRepository)
     {
-        _repository = repository;
+        _produtoRepository = produtoRepository;
+    }
+
+    [HttpGet("produtos/{id}")]
+    public ActionResult<IEnumerable<Produto>> GetProdutosCategoria(int id)
+    {
+        var produtos = _produtoRepository.GetProdutosPorCategoria(id);
+        if (produtos is null)
+            return NotFound();
+
+        return Ok(produtos);
     }
 
     [HttpGet]
     public ActionResult<IEnumerable<Produto>> Get()
     {
-        var produtos = _repository.GetProdutos().ToList();
+        var produtos = _produtoRepository.GetAll();
         
         if (produtos is null)
             return NotFound("Produtos não encontrados"); 
         
-        return produtos;
+        return Ok(produtos);
     }
 
     [HttpGet("{id:int}", Name="ObterProduto")]
     public ActionResult<Produto> Get(int id)
     {
-        var produto = _repository.GetProduto(id);
+        var produto = _produtoRepository.Get(c => c.ProdutoId == id);
         
         if (produto is null)
             return NotFound();
@@ -45,7 +55,7 @@ public class ProdutosController : ControllerBase
         if (produto is null) 
             return BadRequest();
 
-        var novoProduto = _repository.Create(produto);
+        var novoProduto = _produtoRepository.Create(produto);
         
         return new CreatedAtRouteResult("ObterProduto", 
             new { id = produto.ProdutoId }, novoProduto);
@@ -56,32 +66,21 @@ public class ProdutosController : ControllerBase
     {
         if (id != produto.ProdutoId)
             return BadRequest();
-
-        bool atualizado = _repository.Update(produto);
-
-        if (atualizado)
-        {
-            return Ok(produto);
-        }
-        else
-        {
-            return StatusCode(500);
-        }
         
+        var produtoAtualizado = _produtoRepository.Update(produto);
+        
+        return Ok(produtoAtualizado);
     }
 
     [HttpDelete("{id:int}")]
     public ActionResult Delete(int id)
     {
-        bool deletado = _repository.Delete(id);
-        
-        if (deletado)
-        {
-            return Ok();
-        }
-        else
-        {
-            return StatusCode(500);
-        }
+        var produto = _produtoRepository.Get(p => p.ProdutoId == id);
+        if (produto is null)
+            return NotFound();
+
+        _produtoRepository.Delete(produto);
+        return Ok();
+       
     }
 }
