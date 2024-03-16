245c2d8b14be8027cb96136a874169e36d4ab8d3
Matheus Wisch <wisch.pg@outlook.com>
2024-03-16 09:24:56 -0300

    feat: criaçao unit of work

---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using APICatalogo.Context;
+
+namespace APICatalogo.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private IProdutoRepository _produtoRepo;
+    private ICategoriaRepository _categoriaRepo;
+    public AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IProdutoRepository ProdutoRepository
+    {
+        get
+        {
+            return _produtoRepo = _produtoRepo ?? new ProdutoRepository(_context);
+        }
+    }
+    
+    public ICategoriaRepository CategoriaRepository
+    {
+        get
+        {
+            return _categoriaRepo = _categoriaRepo ?? new CategoriasRepository(_context);
+        }
+    }
+
+    public void Commit()
+    {
+        throw new NotImplementedException();
+    }
+}
