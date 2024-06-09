5868a89355e47d435b7312c29c159f841709bd40
Matheus Wisch <wisch.pg@outlook.com>
2024-06-09 16:27:52 -0300

    dev

--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3ec50baf40c969f6ebf52bd49ab4c7b2b6896dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56f462975e97169be65586421a4812467671318")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
